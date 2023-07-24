612806ccd8d5879a5bb55ef5aa16047a2bd62d61
john <johnwokenna@yahoo.com>
2023-07-24 01:03:24 +0100

    My todo on web app

---
+++ webapp1/Todo_on_Web.py
@@ -0,0 +1,31 @@
+import streamlit as St
+import functions
+
+
+#filepath = r"C:\Users\user\My Python\app1\data\todo.txt"
+todos = functions.get_todos()
+
+
+def add_todo():
+    todo_local = St.session_state["new_todo"] + "\n"
+    todos.append(todo_local)
+    functions.write_todos(todos)
+
+
+St.title("My Todo on Web")
+St.subheader("My first app on web")
+St.write("This app is to increase my productivity")
+
+for index, todo in enumerate(todos):
+    checkbox = St.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del St.session_state[todo]
+        St.experimental_rerun()
+
+St.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
+
+
+
